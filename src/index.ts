a13a28395fe939c83cc66336ecf0865fe3031b31
Taiji-Kondo <k.taiji1108@gmail.com>
2023-10-28 23:02:41 +0900

    Delete data witch is not in page's data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
-import * as mysql from 'mysql2';
+import * as mysql from 'mysql2/promise';
 import dotenv from 'dotenv'
+import {RowDataPacket} from "mysql2";
 
 // init dotenv
 dotenv.config()
@@ -24,17 +25,20 @@ type ChairType = {
   price: number
   condition: typeof CONDITION_RANK[keyof typeof CONDITION_RANK]
   inventory: number
+  createdAt: Date
+  updatedAt: Date
+  deletedAt: Date | null
 }
 
-// connect to db
-const connection = mysql.createConnection({
-  host: process.env.DB_ROOT_HOST,
-  user: process.env.DB_ROOT_USER,
-  password: process.env.DB_ROOT_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
 (async () => {
+  // connect to db
+  const connection = await mysql.createConnection({
+    host: process.env.DB_ROOT_HOST,
+    user: process.env.DB_ROOT_USER,
+    password: process.env.DB_ROOT_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
   // const browser = await puppeteer.launch({ headless: true });
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -51,7 +55,12 @@ const connection = mysql.createConnection({
   const resultItems = await resultList?.$$('li.result');
   if (!resultItems) throw new Error('result items not found');
 
-  const data = await page.evaluate((CONDITION_RANK) => {
+  // get current data from db
+  const [rows] = await connection.query<RowDataPacket[]>(`SELECT * FROM chair_embody WHERE deleted_at IS NULL`);
+  const dbChairData: ChairType[] = [...rows as ChairType[]]
+
+  // get latest data from page
+  const latestChairData = await page.evaluate((CONDITION_RANK) => {
     const resultItems = document.querySelectorAll('li.result');
     const resultItemsArray = Array.from(resultItems);
     return resultItemsArray.map((item) => {
@@ -71,27 +80,96 @@ const connection = mysql.createConnection({
         storeName: storeName.trim(),
         price: Number(price.replace(/[^0-9]/g, '')),
         condition,
-        inventory: Number(conditionAndInventory.replace(/[^0-9]/g, ''))
+        inventory: Number(conditionAndInventory.replace(/[^0-9]/g, '')),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null
       }
     }) satisfies ChairType[]
 
   }, CONDITION_RANK)
 
-  // insert data to db
-  const values = data.map(item => [
-    item.id,
-    item.name,
-    item.storeName,
-    item.price,
-    item.condition,
-    item.inventory
-  ])
-  connection.query(
-    'INSERT INTO `chair_embody` (id, name, store_name, price, `condition`, inventory) VALUES ?',
-    [values]
-  );
+  const dbChairDataIds = dbChairData.map((item) => item.id)
+  const latestChairDataIds = latestChairData.map((item) => item.id)
+  const insertData = latestChairData.filter((item) => !dbChairDataIds.includes(item.id)).map((item) => {
+    return [
+      item.id,
+      item.name,
+      item.storeName,
+      item.price,
+      item.condition,
+      item.inventory,
+      item.createdAt,
+      item.updatedAt,
+      item.deletedAt
+    ]
+  })
+  const deleteIds = dbChairDataIds.filter((item) => !latestChairDataIds.includes(item))
+  const deletePlaceholders = deleteIds.map(() => '?').join(',');
+
+  // insert data
+  if (insertData.length > 0){
+    connection.query(
+      `INSERT INTO chair_embody (id, name, store_name, price, \`condition\`, inventory, created_at, updated_at, deleted_at) VALUES ?`,
+      [insertData]
+    );
+  }
+  // delete data which is not in latest data
+  if (deleteIds.length > 0) {
+    connection.query(
+      `DELETE FROM chair_embody WHERE id IN (${deletePlaceholders})`,
+      [deleteIds]
+    );
+  }
 
   // end connection
   connection.end();
   await browser.close();
 })();
+
+const TEST_DATA = [
+  {
+    id: 1,
+    name: 'name1',
+    storeName: 'storeName1',
+    price: 10000,
+    condition: 1,
+    inventory: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null
+  },
+  {
+    id: 2,
+    name: 'name2',
+    storeName: 'storeName2',
+    price: 20000,
+    condition: 2,
+    inventory: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null
+  },
+  {
+    id: 3,
+    name: 'name3',
+    storeName: 'storeName3',
+    price: 30000,
+    condition: 3,
+    inventory: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null
+  },
+  {
+    id: 4,
+    name: 'name4',
+    storeName: 'storeName4',
+    price: 40000,
+    condition: 4,
+    inventory: 4,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null
+  }
+]
