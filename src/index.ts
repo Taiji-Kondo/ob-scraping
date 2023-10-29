d6eae08f10bad19b774a006dc978d421c8d793bb
Taiji-Kondo <k.taiji1108@gmail.com>
2023-10-29 19:30:21 +0900

    Create function of loop get

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import {RowDataPacket} from "mysql2";
 
 /**
  * TODO:
- * - URLを複数設定してmapで回す
  * - 定期実行処理作成
  * - テーブル作成のinitファイルを作成
  * - seedファイルを作成
@@ -15,8 +14,24 @@ import {RowDataPacket} from "mysql2";
 // init dotenv
 dotenv.config()
 
-const EMBODY_CHAIR_URL =
-  'https://www.officebusters.com/kensaku/index_k.php?st=&l=2&m=17&s=&k=&tenpo=&maker=28&condition_rank=4,5&color=1,4&amount=&kakaku_min=&kakaku_max=&dc=3&d_size=&h_size=&w_size=&tag_id=&series_id=2482&mrk=&f_func=&g_size=&cassette=&counter_num=&compound_machine_mono_per_minute=&compound_machine_color_per_minute=&launch_year=&view_mode=&pageNum_kensaku=1&cbl=&cbm=&cbs=&level=s';
+const PRODUCTS = {
+  EMBODY_CHAIR: {
+    url: 'https://www.officebusters.com/kensaku/index_k.php?st=&l=2&m=17&s=&k=&tenpo=&maker=28&condition_rank=4,5&color=1,4&amount=&kakaku_min=&kakaku_max=&dc=3&d_size=&h_size=&w_size=&tag_id=&series_id=2482&mrk=&f_func=&g_size=&cassette=&counter_num=&compound_machine_mono_per_minute=&compound_machine_color_per_minute=&launch_year=&view_mode=&pageNum_kensaku=1&cbl=&cbm=&cbs=&level=s',
+    name: 'Herman Miller Embody Chair',
+    tableName: 'chair_embody',
+  },
+  AERON_CHAIR: {
+    url: 'https://www.officebusters.com/kensaku/index_k.php?st=6&l=2&m=17&s=&k=&tenpo=&maker=28&condition_rank=4,5&color=&amount=&kakaku_min=&kakaku_max=&dc=3&d_size=&h_size=&w_size=&tag_id=&series_id=1490&mrk=80&f_func=&g_size=&cassette=&counter_num=&compound_machine_mono_per_minute=&compound_machine_color_per_minute=&launch_year=&view_mode=photo&pageNum_kensaku=1&cbl=&cbm=&cbs=&level=s',
+    name: 'Herman Miller Aeron Chair',
+    tableName: 'chair_aeron',
+  },
+  CONTESSA_CHAIR: {
+    url: 'https://www.officebusters.com/kensaku/index_k.php?st=&l=2&m=17&s=&k=&tenpo=&maker=28&condition_rank=4,5&color=1,4&amount=&kakaku_min=&kakaku_max=&dc=3&d_size=&h_size=&w_size=&tag_id=&series_id=2482&mrk=&f_func=&g_size=&cassette=&counter_num=&compound_machine_mono_per_minute=&compound_machine_color_per_minute=&launch_year=&view_mode=&pageNum_kensaku=1&cbl=&cbm=&cbs=&level=s',
+    name: 'OKAMURA Contessa',
+    tableName: 'chair_contessa',
+  },
+} as const satisfies Record<string, { url: string, name: string, tableName: string }>
+
 const CONDITION_RANK = ['Ｓ', 'Ａ', 'Ｂ＋', 'Ｂ', 'Ｃ＋', 'Ｃ', 'Ｄ',] as const
 
 type ChairType = {
@@ -63,88 +78,92 @@ const notifySlack = async (message: string) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto(EMBODY_CHAIR_URL);
+  const messages: string[] = []
+  for await (const {name, url, tableName} of Object.values(PRODUCTS)) {
+    await page.goto(url);
 
-  // select corporate or individual
-  const individualButton = await page.waitForSelector('li[data-section="indivi"] > a');
-  if (!individualButton) throw new Error('individual button not found');
-  await individualButton.click();
+    // select corporate or individual
+    const individualButton = await page.$('li[data-section="indivi"] > a');
+    await individualButton?.click();
 
-  // get page results
-  const resultList = await page.waitForSelector('ul.result-group');
-  const resultItems = await resultList?.$$('li.result');
-  if (!resultItems) throw new Error('result items not found');
+    // get page results
+    const resultList = await page.waitForSelector('ul.result-group');
+    const resultItems = await resultList?.$$('li.result');
+    if (!resultItems) throw new Error('result items not found');
 
-  // get current data from db
-  const [rows] = await connection.query<RowDataPacket[]>(`SELECT * FROM chair_embody WHERE deleted_at IS NULL`);
-  const dbChairData: ChairType[] = [...rows as ChairType[]]
+    // get current data from db
+    const [rows] = await connection.query<RowDataPacket[]>(`SELECT * FROM ${tableName} WHERE deleted_at IS NULL`);
+    const dbChairData: ChairType[] = [...rows as ChairType[]]
 
-  // get latest data from page
-  const latestChairData = await page.evaluate((CONDITION_RANK) => {
-    const resultItems = document.querySelectorAll('li.result');
-    const resultItemsArray = Array.from(resultItems);
-    return resultItemsArray.map((item) => {
-      const id = item.querySelector('p.result_detail_card_category-and-id_prod-id')?.textContent
-      const name= item.querySelector('li.result_detail_card_series')?.textContent
-      const storeName= item.querySelector('li.result_detail_card_tenpo')?.textContent
-      const url = item.querySelector('a[href]')?.getAttribute('href')
-      const price= item.querySelector('li.result_detail_card_price')?.textContent
-      const conditionAndInventory= item.querySelector('p.result_detail_card_bottom_state')?.textContent
-      if (!id || !name || !storeName || !url || !price || !conditionAndInventory) throw new Error('result item not found');
+    // get latest data from page
+    const latestChairData = await page.evaluate((CONDITION_RANK) => {
+      const resultItems = document.querySelectorAll('li.result');
+      const resultItemsArray = Array.from(resultItems);
+      return resultItemsArray.map((item) => {
+        const id = item.querySelector('p.result_detail_card_category-and-id_prod-id')?.textContent
+        const name = item.querySelector('li.result_detail_card_series')?.textContent
+        const storeName = item.querySelector('li.result_detail_card_tenpo')?.textContent
+        const url = item.querySelector('a[href]')?.getAttribute('href')
+        const price = item.querySelector('li.result_detail_card_price')?.textContent
+        const conditionAndInventory = item.querySelector('p.result_detail_card_bottom_state')?.textContent
+        if (!id || !name || !storeName || !url || !price || !conditionAndInventory) throw new Error('result item not found');
 
-      const trimmedCondition = conditionAndInventory.replace('状態', '').replace('｜在庫数', '').replace(/[0-9]/g, '').trim()
-      const condition = CONDITION_RANK.indexOf(trimmedCondition as typeof CONDITION_RANK[number])
+        const trimmedCondition = conditionAndInventory.replace('状態', '').replace('｜在庫数', '').replace(/[0-9]/g, '').trim()
+        const condition = CONDITION_RANK.indexOf(trimmedCondition as typeof CONDITION_RANK[number])
 
-      return {
-        id: Number(id),
-        name: name,
-        storeName: storeName.trim(),
-        url: url,
-        price: Number(price.replace(/[^0-9]/g, '')),
-        condition,
-        inventory: Number(conditionAndInventory.replace(/[^0-9]/g, '')),
-      }
-    }) satisfies ChairType[]
+        return {
+          id: Number(id),
+          name: name,
+          storeName: storeName.trim(),
+          url: url,
+          price: Number(price.replace(/[^0-9]/g, '')),
+          condition,
+          inventory: Number(conditionAndInventory.replace(/[^0-9]/g, '')),
+        }
+      }) satisfies ChairType[]
 
-  }, CONDITION_RANK)
+    }, CONDITION_RANK)
 
-  const dbChairDataIds = dbChairData.map((item) => item.id)
-  const latestChairDataIds = latestChairData.map((item) => item.id)
-  const insertData = latestChairData.filter((item) => !dbChairDataIds.includes(item.id)).map((item) => {
-    return [
-      item.id,
-      item.name,
-      item.storeName,
-      item.url,
-      item.price,
-      item.condition,
-      item.inventory,
-    ]
-  })
-  const deleteIds = dbChairDataIds.filter((item) => !latestChairDataIds.includes(item))
-  const deletePlaceholders = deleteIds.map(() => '?').join(',');
+    const dbChairDataIds = dbChairData.map((item) => item.id)
+    const latestChairDataIds = latestChairData.map((item) => item.id)
+    const insertData = latestChairData.filter((item) => !dbChairDataIds.includes(item.id)).map((item) => {
+      return [
+        item.id,
+        item.name,
+        item.storeName,
+        item.url,
+        item.price,
+        item.condition,
+        item.inventory,
+      ]
+    })
+    const deleteIds = dbChairDataIds.filter((item) => !latestChairDataIds.includes(item))
+    const deletePlaceholders = deleteIds.map(() => '?').join(',');
 
-  // insert data
-  if (insertData.length > 0){
-    connection.query(
-      `INSERT INTO chair_embody (id, name, store_name, url, price, \`condition\`, inventory) VALUES ?`,
-      [insertData]
-    );
-  }
-  // delete data which is not in latest data
-  if (deleteIds.length > 0) {
-    connection.query(
-      `DELETE FROM chair_embody WHERE id IN (${deletePlaceholders})`,
-      [deleteIds]
-    );
+    // insert data
+    if (insertData.length > 0) {
+      connection.query(
+        `INSERT INTO ${tableName} (id, name, store_name, url, price, \`condition\`, inventory) VALUES ?`,
+        [insertData]
+      );
+    }
+    // delete data which is not in latest data
+    if (deleteIds.length > 0) {
+      connection.query(
+        `DELETE FROM ${tableName} WHERE id IN (${deletePlaceholders})`,
+        [deleteIds]
+      );
+    }
+
+    if (insertData.length > 0) {
+      const message = `${name}に新着商品が${insertData.length}件あります\n
+      ${insertData.map((item) => `id: ${item[0]}, price: ${item[4].toLocaleString()}円, condition: ${CONDITION_RANK[item[5] as number]}, url: ${item[3]}`).join('\n')}`
+      messages.push(message)
+    }
   }
 
   // notify slack
-  // TODO: スクレイピング時にURLを取得して、URLを通知する
-  const message = `新着商品が${insertData.length}件あります\n
-  ${insertData.map((item) => `id: ${item[0]}, price: ${item[4].toLocaleString()}円, condition: ${CONDITION_RANK[item[5] as number]}, url: ${item[3]}`).join('\n')}`
-  console.log(message)
-  await notifySlack(message)
+  await notifySlack(messages.join('\n\n'))
 
   // end connection
   connection.end();
